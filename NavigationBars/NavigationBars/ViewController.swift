19d1ebbdeb79cadfd7f13fe2492cc2a25832f6fe
Joey <joey.hernandez33@gmail.com>
2016-04-14 21:23:32 -0600

    Added stop watch functionality

--- NavigationBars/NavigationBars/ViewController.swift
+++ NavigationBars/NavigationBars/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime()
+    {
+        count += 1
+        time.text = "\(count)"
+    }
+    
+    @IBOutlet var time: UILabel!
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector:#selector(ViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+        count = 0
+        time.text = "0"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
